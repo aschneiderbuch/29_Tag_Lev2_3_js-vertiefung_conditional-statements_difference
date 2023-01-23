7b6f70b732ca3a7d9e4e013d0ba54b23ff16c7b2
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-23 18:03:42 +0100

    testingPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
 console.log("test?");
+
+/* 
+Frage
+
+function differenz
+
+x = ?
+y = 27
+
+x > 27  dann (x-y)*2
+
+testen mit x 35 74 123
+
+*/
+
+let x = 0
+let y = 27
+
+function differenz(x, y) {
+    let ergebnis = 0;
+
+    if (x > y) {
+        ergebnis   = (x-y)*2 
+        console.log("Differenz: " + (x-y));
+    }
+    else {
+        ergebnis   = (y-x)
+    }
+
+    console.log(ergebnis)
+}
+
+differenz(x,y);
